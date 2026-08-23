107e0acfb04dad3baa26d4cf4703afcb4154ca67
windsing <mw@windsing.cn>
2026-08-23 13:36:42 +0800

    chore: remove debug file

--- test/testdata_perf/_check.sql
+++
@@ -1,3 +0,0 @@
-SET aligned_data_root='D:/proj/factorlake/testdata_perf';
-ATTACH 'D:/proj/factorlake/testdata_perf' AS al (TYPE ALIGNED);
-SELECT * FROM aligned_groups('perf_test', 'D:/proj/factorlake/testdata_perf');
